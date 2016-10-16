6decb642971fdce83a663fcb50474f8a25005697
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2016-10-16 20:23:30 +1100

    Requiring logged in users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
 
   # Retunrs true if the given user is the current user.
   def current_user?(user)
-    user = current_user
+    user == current_user
   end
 
   # Returns the user corresponding to the remmeber token cookie.
@@ -48,5 +48,17 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 
 end
