fa62a5767ac6f1e8af0ec74546fe4be2836a50f3
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2016-10-21 17:16:14 +1100

    Generalise the authenticated? method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -74,7 +74,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')  # Left this blank as the error occurs before it gets used.
+    assert_not @user.authenticated?(:remember, '')  # Left this blank as the error occurs before it gets used.
   end
 
 end
