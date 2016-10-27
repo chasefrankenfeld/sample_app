4f99ed2819a5c0796e1ee43f74d9faa28db5040e
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2016-10-27 14:23:49 +1100

    Add reset password capability

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,8 +53,7 @@ class User < ApplicationRecord
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # Sends password reset email.
@@ -62,6 +61,10 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   private
 
