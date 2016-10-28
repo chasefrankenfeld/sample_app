faaa517004c5f605e2b5078aaf7ab52d0a7c8d7e
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2016-10-28 17:24:18 +1100

    Display microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')  # Left this blank as the error occurs before it gets used.
   end
 
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
