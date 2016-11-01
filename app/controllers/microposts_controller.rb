356853b967f3e84aef9510f7094cc1b30dabd425
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2016-11-01 15:36:43 +1100

    Add user microposts and set up for AJAX to edit the microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,8 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy]
-  before_action :correct_user,   only: [:destroy]
+  #include ActionController::MimeResponds
+  before_action :logged_in_user, only: [:create, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+  #respond_to :html, :js
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -10,6 +12,24 @@ class MicropostsController < ApplicationController
     else
       @feed_items = []
       render 'static_pages/home'
+    end
+  end
+
+  # This can be romved as Rails can generate this for me
+  def edit
+    #respond_to do |format|
+    #  format.html
+    #  format.json
+    #end
+  end
+
+  def update
+    if current_user?(@micropost.user)
+      @micropost.update_attributes(micropost_params)
+      redirect_to root_url
+      flash[:success] = "Micropost updated"
+    else
+      render 'edit'
     end
   end
 
